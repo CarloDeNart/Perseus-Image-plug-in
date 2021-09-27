1f734e8c06f2805297a000233ba962fd5c63ae60
CarloDeNart <82389414+CarloDeNart@users.noreply.github.com>
2021-09-27 11:36:09 +0200

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -2,9 +2,9 @@
 
 # -- Project information
 
-project = 'Lumache'
-copyright = '2021, Graziella'
-author = 'Graziella'
+project = 'Perseus Image plug-in'
+copyright = '2021, Coxlab'
+author = 'Bela Frohn, Carlo De Nart'
 
 release = '0.1'
 version = '0.1.0'
